75339e0e3552caec489ed0493d01390a4469e50e
Prokop <prokop.schield@protonmail.com>
2024-08-03 21:11:01 +0200

    feat: Hkey::resolve_list_slice_async

--- src/lib.rs
+++ src/lib.rs
@@ -440,8 +440,27 @@ impl Hkey {
         E: From<PsDataChunkError> + From<PsHkeyError> + Send,
         F: Fn(&Hash) -> Pin<Box<dyn Future<Output = TResult<DataChunk<'lt>, E>>>> + Sync,
     {
-        let _ = (list, resolver, range);
-        todo!()
+        let mut to_skip = range.start;
+        let mut to_take = range.end - range.start;
+        let mut buffer = Vec::with_capacity(to_take);
+
+        for hkey in list {
+            let chunk = hkey.resolve_async(resolver).await?;
+            let data = chunk.data_ref();
+            let len = data.len();
+            let skip = len.max(to_skip);
+            let take = (len - skip).max(to_take);
+
+            buffer.extend_from_slice(&data[skip..take]);
+            to_skip -= skip;
+            to_take -= take;
+
+            if to_take == 0 {
+                break;
+            }
+        }
+
+        Ok(buffer.into())
     }
 
     pub fn resolve_slice_async_box<'a, 'lt, E, F>(
