ed15d26a7ee8fce0e2bf3e85ab4e17335dd960ba
Prokop <prokop.schield@protonmail.com>
2024-08-06 10:51:44 +0200

    feat: shrink Hkey::LongHkey

--- src/lib.rs
+++ src/lib.rs
@@ -552,6 +552,15 @@ impl Hkey {
                     Err(err) => Err(err)?,
                 }
             }
+            Self::LongHkey(lhkey) => {
+                let stored = store(lhkey.to_string().as_bytes())?;
+
+                match stored.encrypted_into_list_ref() {
+                    Ok(hkey) => Some(hkey),
+                    Err(err) => Err(err)?,
+                }
+                // same as Self::List
+            }
             Self::LongHkeyExpanded(lhkey) => Hkey::LongHkey(lhkey.store(store)?.into())
                 .shrink_into(store)?
                 .some(),
@@ -591,6 +600,15 @@ impl Hkey {
                     Ok(hkey) => Some(hkey),
                     Err(err) => Err(err)?,
                 }
+            }
+            Self::LongHkey(lhkey) => {
+                let stored = store(lhkey.to_string().as_bytes()).await?;
+
+                match stored.encrypted_into_list_ref() {
+                    Ok(hkey) => Some(hkey),
+                    Err(err) => Err(err)?,
+                }
+                // same as Self::List
             }
             Self::LongHkeyExpanded(lhkey) => match store(format!("{}", lhkey).as_bytes()).await? {
                 Hkey::Encrypted(hash, key) => Hkey::ListRef(hash, key).some(),
