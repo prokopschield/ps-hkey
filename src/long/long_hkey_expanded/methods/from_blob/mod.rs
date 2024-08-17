73fb024f42d6bb8a5adb574699e2d2f830927376
Prokop <prokop.schield@protonmail.com>
2024-08-17 20:48:33 +0200

    wip: LongHkeyExpanded::from_blob

---
+++ src/long/long_hkey_expanded/methods/from_blob/mod.rs
@@ -0,0 +1,53 @@
+use std::sync::Arc;
+
+use ps_datachunk::PsDataChunkError;
+use rayon::{
+    iter::{IndexedParallelIterator, ParallelIterator},
+    slice::ParallelSlice,
+};
+
+use crate::{
+    long::{
+        long_hkey_expanded::{
+            constants::LHKEY_LEVEL_MAX_LENGTH,
+            methods::update::helpers::{calculate_depth, calculate_segment_length},
+        },
+        LongHkeyExpanded,
+    },
+    Hkey, PsHkeyError, Range,
+};
+
+impl LongHkeyExpanded {
+    pub fn from_blob<'lt, E, Es, S>(store: &S, data: &[u8]) -> Result<Self, E>
+    where
+        E: From<Es> + From<PsHkeyError> + From<PsDataChunkError> + Send,
+        Es: Into<E> + Send,
+        S: Fn(&[u8]) -> Result<Hkey, Es> + Sync,
+    {
+        let depth = calculate_depth(0, data.len());
+
+        let parts: Result<Vec<(Range, Hkey)>, E> = if data.len() > LHKEY_LEVEL_MAX_LENGTH {
+            let segment_length = calculate_segment_length(depth);
+
+            let chunks = data.par_chunks(segment_length);
+
+            chunks
+                .enumerate()
+                .map(|(index, chunk)| {
+                    let start = index * segment_length;
+                    let end = start + chunk.len();
+                    let hkey = LongHkeyExpanded::from_blob(store, chunk)?.shrink(store)?;
+
+                    Ok((start..end, hkey))
+                })
+                .collect()
+        } else {
+            todo!()
+        };
+
+        let parts = Arc::from(parts?.into_boxed_slice());
+        let lhkey = LongHkeyExpanded::new(depth, data.len(), parts);
+
+        Ok(lhkey)
+    }
+}
--- src/long/long_hkey_expanded/methods/mod.rs
+++ src/long/long_hkey_expanded/methods/mod.rs
@@ -1,3 +1,4 @@
+pub mod from_blob;
 pub mod normalize_segment;
 pub mod shrink;
 pub mod update;
