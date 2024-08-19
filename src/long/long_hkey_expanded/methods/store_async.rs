eb62d2b973176eb41482a6da65b79f4b68ce4c9c
Prokop <prokop.schield@protonmail.com>
2024-08-19 20:02:05 +0200

    refactor: add Ef parameter to LongHkeyExpanded::store_async

--- src/long/long_hkey_expanded/methods/store_async.rs
+++ src/long/long_hkey_expanded/methods/store_async.rs
@@ -5,11 +5,12 @@ use ps_util::ToResult;
 use crate::{Hkey, LongHkey, LongHkeyExpanded, PsHkeyError};
 
 impl LongHkeyExpanded {
-    pub async fn store_async<'lt, E, F, Ff>(&self, store: &F) -> Result<LongHkey, E>
+    pub async fn store_async<'lt, E, Ef, F, Ff>(&self, store: &F) -> Result<LongHkey, E>
     where
-        E: From<PsHkeyError> + Send,
+        E: From<Ef> + From<PsHkeyError> + Send,
+        Ef: Into<E> + Send,
         F: Fn(&[u8]) -> Ff,
-        Ff: Future<Output = Result<Hkey, E>> + Sync,
+        Ff: Future<Output = Result<Hkey, Ef>> + Sync,
     {
         match store(self.to_string().as_bytes()).await? {
             Hkey::Encrypted(hash, key) => LongHkey::from_hash_and_key(hash, key),
