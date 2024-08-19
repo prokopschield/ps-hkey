f7ef53f840d41550b1db7df6a1497288af4c47e1
Prokop <prokop.schield@protonmail.com>
2024-08-19 20:30:43 +0200

    feat: LongHkeyExpanded::shrink_async

--- src/long/long_hkey_expanded/methods/mod.rs
+++ src/long/long_hkey_expanded/methods/mod.rs
@@ -1,6 +1,7 @@
 pub mod from_blob;
 pub mod normalize_segment;
 pub mod shrink;
+pub mod shrink_async;
 pub mod store;
 pub mod store_async;
 pub mod update;
---
+++ src/long/long_hkey_expanded/methods/shrink_async.rs
@@ -0,0 +1,16 @@
+use std::future::Future;
+
+use crate::{long::LongHkeyExpanded, Hkey, PsHkeyError};
+
+impl LongHkeyExpanded {
+    /// transforms this LongHkey into a Hkey::ListRef
+    pub async fn shrink_async<E, Ef, F, Ff>(&self, store: &F) -> Result<Hkey, E>
+    where
+        E: From<Ef> + From<PsHkeyError> + Send,
+        Ef: Into<E> + Send,
+        F: Fn(&[u8]) -> Ff,
+        Ff: Future<Output = Result<Hkey, Ef>> + Sync,
+    {
+        Ok(self.store_async(store).await?.into())
+    }
+}
