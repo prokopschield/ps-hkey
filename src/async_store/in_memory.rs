6e03d803c7295a25b71433607ff5bf38e8ec4e65
Prokop <prokop.schield@protonmail.com>
2025-07-16 13:56:00 +0200

    refactor: AsyncStore error type must be PromiseRejection

--- src/async_store/in_memory.rs
+++ src/async_store/in_memory.rs
@@ -1,7 +1,6 @@
-
 use ps_datachunk::OwnedDataChunk;
 use ps_hash::Hash;
-use ps_promise::Promise;
+use ps_promise::{Promise, PromiseRejection};
 
 use crate::{
     store::in_memory::{InMemoryStore, InMemoryStoreError},
@@ -17,17 +16,31 @@ pub struct InMemoryAsyncStore {
 
 impl AsyncStore for InMemoryAsyncStore {
     type Chunk = OwnedDataChunk;
-    type Error = InMemoryStoreError;
+    type Error = InMemoryAsyncStoreError;
 
     fn get(&self, hash: &Hash) -> Promise<Self::Chunk, Self::Error> {
         let result = self.store.get(hash);
 
-        Promise::new(async move { result })
+        Promise::new(async move { Ok(result?) })
     }
 
     fn put(&self, bytes: &[u8]) -> Promise<Hkey, Self::Error> {
         let result = self.store.put(bytes);
 
-        Promise::new(async move { result })
+        Promise::new(async move { Ok(result?) })
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum InMemoryAsyncStoreError {
+    #[error("The Promise was consumed more than once.")]
+    PromiseConsumedAlready,
+    #[error(transparent)]
+    StoreError(#[from] InMemoryStoreError),
+}
+
+impl PromiseRejection for InMemoryAsyncStoreError {
+    fn already_consumed() -> Self {
+        Self::PromiseConsumedAlready
     }
 }
--- src/async_store/mod.rs
+++ src/async_store/mod.rs
@@ -1,16 +1,14 @@
 pub mod in_memory;
 
-use std::error::Error;
-
 use ps_datachunk::DataChunk;
 use ps_hash::Hash;
-use ps_promise::Promise;
+use ps_promise::{Promise, PromiseRejection};
 
 use crate::Hkey;
 
 pub trait AsyncStore {
     type Chunk: DataChunk + Unpin;
-    type Error: Error + Unpin;
+    type Error: PromiseRejection + Unpin;
 
     fn get(&self, hash: &Hash) -> Promise<Self::Chunk, Self::Error>;
     fn put(&self, data: &[u8]) -> Promise<Hkey, Self::Error>;
